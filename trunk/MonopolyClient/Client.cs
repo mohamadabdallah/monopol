7115a1451fc3b45fdd065aa5ab0a39fcd892c210
michaltomczak <michaltomczak@eafba34f-6b24-0410-95c3-63684ad52ad7>
2007-01-09 12:00:16 +0000

    Dodaje klienta

---
+++ trunk/MonopolyClient/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using Monopoly;
+
+namespace MonopolyClient
+{
+    class Client
+    {
+        #region Private Variables
+
+        Socket socket;
+
+        #endregion
+        #region Public Constructors
+
+        public Client(IPAddress IPAdress, int aPort, string aNick)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(IPAdress, aPort); 
+        }
+
+        #endregion
+    }
+}
